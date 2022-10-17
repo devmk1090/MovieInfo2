f6dc1dbae560f62f8f2a9ae20ae5c38c681ca3b7
devmk1090 <devmk1090@gmail.com>
2022-10-18 03:30:08 +0900

    Change 'android:Theme.Material.Light.NoActionBar' to 'Theme.MaterialComponents.Light.NoActionBar' and Add SplashActivity

---
+++ app/src/main/java/com/devkproject/movieinfo2/MainFrame.kt
@@ -0,0 +1,8 @@
+package com.devkproject.movieinfo2
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainFrame() {
+
+}
---
+++ app/src/main/java/com/devkproject/movieinfo2/SplashActivity.kt
@@ -0,0 +1,44 @@
+package com.devkproject.movieinfo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.devkproject.movieinfo2.ui.theme.MovieInfo2Theme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MovieInfo2Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainName("MovieInfo2")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainName(name: String) {
+    Text(name)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashPreview() {
+    MovieInfo2Theme {
+        MainName("MovieInfo2")
+    }
+}

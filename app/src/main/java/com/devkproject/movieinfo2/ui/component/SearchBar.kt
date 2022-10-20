890b95225745bee69efe7e6683dd88193a21c778
devmk1090 <devmk1090@gmail.com>
2022-10-21 01:49:48 +0900

    Make AppBarWithArrow.kt component and Add SearchBar.kt code

--- app/src/main/java/com/devkproject/movieinfo2/ui/component/SearchBar.kt
+++ app/src/main/java/com/devkproject/movieinfo2/ui/component/SearchBar.kt
@@ -1,16 +1,23 @@
 package com.devkproject.movieinfo2.ui.component
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.devkproject.movieinfo2.ui.MainViewModel
 import com.devkproject.movieinfo2.ui.theme.blue
@@ -39,7 +46,31 @@ fun SearchBar(isAppBarVisible: MutableState<Boolean>, viewModel: MainViewModel) 
                 text = it
             },
             singleLine = true,
+            trailingIcon = {
+                if (text.trim().isNotEmpty()) {
+                    Icon(
+                        Icons.Filled.Clear,
+                        contentDescription = "empty text",
+                        modifier = Modifier
+                            .padding(end = 16.dp)
+                            .clickable {
+                                text = ""
+                            }
+                    )
+                } else {
+                    Icon(
+                        Icons.Filled.Search,
+                        contentDescription = "search",
+                        modifier = Modifier
+                            .padding(end = 16.dp)
+                            .clickable { }
+                    )
+                }
+            }
         )
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+        }
     }
 }
 

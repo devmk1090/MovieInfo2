bc6a22f89fc12f649749b719e3102fd13ccacc17
devmk1090 <devmk1090@gmail.com>
2022-10-23 01:26:03 +0900

    Make ExitAlertDialog compose view

--- app/src/main/java/com/devkproject/movieinfo2/ui/component/appbar/SearchBar.kt
+++ app/src/main/java/com/devkproject/movieinfo2/ui/component/appbar/SearchBar.kt
@@ -1,4 +1,4 @@
-package com.devkproject.movieinfo2.ui.component
+package com.devkproject.movieinfo2.ui.component.appbar
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
--- app/src/main/java/com/devkproject/movieinfo2/ui/component/screens/SplashActivity.kt
+++ app/src/main/java/com/devkproject/movieinfo2/ui/component/screens/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.devkproject.movieinfo2.ui
+package com.devkproject.movieinfo2.ui.component.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle

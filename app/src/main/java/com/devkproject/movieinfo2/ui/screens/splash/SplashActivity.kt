02e8039d3ca2197469db6ae442c9fd25f63e3b6b
devmk1090 <devmk1090@gmail.com>
2022-10-25 02:15:24 +0900

    - One of the changes in Android 12, SplashScren was applied. - SplashScreen's function 1. windowSpalshScreenBackground: Specify background color 2. windowSplashScreenAnimatedIcon: Center the icon on the screen 3. windowSplashScreenAnimationDuration: Specify the duration of the splash screen 4. postSplashScreenTheme: Specify theme

--- app/src/main/java/com/devkproject/movieinfo2/ui/screens/splash/SplashActivity.kt
+++ app/src/main/java/com/devkproject/movieinfo2/ui/screens/splash/SplashActivity.kt
@@ -1,8 +1,9 @@
-package com.devkproject.movieinfo2.ui.screens
+package com.devkproject.movieinfo2.ui.screens.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,13 +11,20 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.devkproject.movieinfo2.ui.theme.MovieInfo2Theme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+
+    private val splashViewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { splashViewModel.isLoading.value }
+        }
         setContent {
             MovieInfo2Theme {
                 Surface(

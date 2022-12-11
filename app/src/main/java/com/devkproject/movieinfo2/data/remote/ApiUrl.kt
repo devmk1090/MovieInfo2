66bdaf2dc6bf8df9f1837c86e2a9b971e0f40e3b
devmk1090 <devmk1090@gmail.com>
2022-12-12 01:45:57 +0900

    Change language en to ko

--- app/src/main/java/com/devkproject/movieinfo2/data/remote/ApiUrl.kt
+++ app/src/main/java/com/devkproject/movieinfo2/data/remote/ApiUrl.kt
@@ -5,24 +5,24 @@ object ApiUrl {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val POSTER_URL = "https://image.tmdb.org/t/p/w342"
 
-    const val MOVIE_LIST = "movie/now_playing?api_key=$API_KEY&language=en-US"
-    const val POPULAR_MOVIE_LIST = "movie/popular?api_key=$API_KEY&language=en-US"
-    const val TOP_RATED_MOVIE_LIST = "movie/top_rated?api_key=$API_KEY&language=en-US"
-    const val UP_COMING_MOVIE_LIST = "movie/upcoming?api_key=$API_KEY&language=en-US"
+    const val MOVIE_LIST = "movie/now_playing?api_key=$API_KEY&language=ko"
+    const val POPULAR_MOVIE_LIST = "movie/popular?api_key=$API_KEY&language=ko"
+    const val TOP_RATED_MOVIE_LIST = "movie/top_rated?api_key=$API_KEY&language=ko"
+    const val UP_COMING_MOVIE_LIST = "movie/upcoming?api_key=$API_KEY&language=ko"
 
-    const val MOVIE_DETAIL ="movie/{movieId}?api_key=$API_KEY&language=en-US"
+    const val MOVIE_DETAIL ="movie/{movieId}?api_key=$API_KEY&language=ko"
 
-    const val GENRE_LIST ="genre/movie/list?api_key=$API_KEY&language=en-US"
-    const val GENRE_MOVIES_BY_ID ="discover/movie?api_key=$API_KEY&language=en-US"
+    const val GENRE_LIST ="genre/movie/list?api_key=$API_KEY&language=ko"
+    const val GENRE_MOVIES_BY_ID ="discover/movie?api_key=$API_KEY&language=ko"
 
-    const val SEARCH_MOVIE ="search/movie?api_key=$API_KEY&language=en-US&page=1&include_adult=false"
+    const val SEARCH_MOVIE ="search/movie?api_key=$API_KEY&language=ko&page=1&include_adult=false"
 
-    const val MOVIE_VIDEO ="movie/{movieId}/videos?api_key=$API_KEY&language=en-US"
+    const val MOVIE_VIDEO ="movie/{movieId}/videos?api_key=$API_KEY&language=ko"
 
-    const val RECOMMENDED_MOVIE ="movie/{movieId}/recommendations?api_key=$API_KEY&language=en-US"
+    const val RECOMMENDED_MOVIE ="movie/{movieId}/recommendations?api_key=$API_KEY&language=ko"
 
-    const val MOVIE_CREDIT ="movie/{movieId}/credits?api_key=$API_KEY&language=en-US"
-    const val ARTIST_DETAIL ="person/{personId}?api_key=$API_KEY&language=en-US"
+    const val MOVIE_CREDIT ="movie/{movieId}/credits?api_key=$API_KEY&language=ko"
+    const val ARTIST_DETAIL ="person/{personId}?api_key=$API_KEY&language=ko"
 
     //Youtube
     const val YOUTUBE_BASE_URL = "https://www.youtube.com/watch?v="
